e8f5e75c83cec636bee4a8a414c599ba5b7bfb72
Renato_AD <renatoaugustod@gmail.com>
2026-02-11 23:20:23 -0300

    Padroniza botões do cabeçalho com ícones

--- tests/test_export_button_position.py
+++ tests/test_export_button_position.py
@@ -9,7 +9,6 @@ from csv_store import CsvStore
 QApplication = qtwidgets.QApplication
 QToolButton = qtwidgets.QToolButton
 QLabel = qtwidgets.QLabel
-QPushButton = qtwidgets.QPushButton
 
 
 def _get_app():
@@ -24,26 +23,18 @@ def test_shortcuts_section_renders_buttons_above_tabs(tmp_path):
     store = CsvStore(str(tmp_path))
     win = MainWindow(store)
 
-    section_label = win.findChild(QLabel, "shortcutsSectionLabel")
-    assert section_label is not None
-    assert section_label.text() == "Atalhos"
-
-    export_label = win.findChild(QLabel, "exportShortcutLabel")
-    assert export_label is not None
-    assert export_label.text() == "Exportar"
-
     export_button = win.findChild(QToolButton, "exportAction")
     assert export_button is not None
 
-    primary_button = win.findChild(QPushButton, "primaryAction")
-    danger_button = win.findChild(QPushButton, "dangerAction")
+    primary_button = win.findChild(QToolButton, "primaryAction")
+    danger_button = win.findChild(QToolButton, "dangerAction")
     assert primary_button is not None
     assert danger_button is not None
 
     for i in range(win.t1_actions_layout.count()):
         item = win.t1_actions_layout.itemAt(i)
         widget = item.widget()
-        if isinstance(widget, QPushButton):
-            assert widget.objectName() not in {"primaryAction", "dangerAction"}
+        if isinstance(widget, QToolButton):
+            assert widget.objectName() not in {"primaryAction", "dangerAction", "exportAction"}
 
     win.close()
